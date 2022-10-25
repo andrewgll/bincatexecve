55b8ca7ecf79e1f275d3b469d4c8e72b5a1491ff
a <a@localhost.localdomain>
2022-10-25 03:00:59 +0300

    Added assembly code

---
+++ code.s
@@ -0,0 +1,21 @@
+global  _start
+section .text
+_start:
+
+xor     rax, rax
+push    rax
+mov     rax, 0x7478742E67616C66 ;txt.galf
+push    rax
+mov     rcx, rsp
+xor     rax, rax
+push    rax
+mov     rax, 0x7461632F6E69622F ;tac/nib/
+push    rax
+mov     rdi, rsp
+xor     rax, rax
+push    rax
+push    rcx
+push    rdi
+mov     rsi, rsp
+mov     al, 0x3b
+syscall
